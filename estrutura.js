4b3d39e2261a433e09afbc40bf2b7034d6c8e9d0
CaioSilva-hub <caio.csbb@gmail.com>
2022-02-25 13:19:16 -0300

    exercício estrutura de dados

---
+++ estrutura.js
@@ -0,0 +1,39 @@
+const criancas = [
+    {
+        aluno: 'Lana',
+        mae: 'Poly', 
+        brincadeira_favorita: 'morder as coisas',    
+        idade: 1,
+        tem_irmaos: false,
+        come_sozinho: false,   
+        melhor_amigo: {
+            nome: 'Sônia',
+            idade: '62'
+        }
+    },
+    {
+        aluno: 'Miguel',
+        mae: 'Prisciane', 
+        brincadeira_favorita: 'brincar com animais',    
+        idade: 4,
+        tem_irmaos: false,
+        come_sozinho: true,   
+        melhor_amigo: {
+            nome: 'Livia',
+            idade: '6'
+        }
+    },
+    {
+        aluno: 'Livia',
+        mae: 'Mariane', 
+        brincadeira_favorita: 'brincar com boneca',    
+        idade: 5,
+        tem_irmaos: true,
+        come_sozinho: true,   
+        melhor_amigo: {
+            nome: 'Isabele',
+            idade: '3'
+        }
+    },
+
+]
