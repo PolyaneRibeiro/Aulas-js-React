bb44c8b9c2622d658a740afeadd26bb9de0c0048
Polyane <poly.pequena@gmail.com>
2022-03-18 01:47:15 -0300

    filter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,55 +5,87 @@ function App() {
   const familia = [
     {
       nome: 'José Carlos',
-      idade: 62,
       dataNascimento: '12/03/1958',
       nomeMae: 'Maria',
-      nomePai: false
+      aposentado: true
     },
     {
       nome: 'Sônia Maria',
-      idade: 61,
       dataNascimento: '03/09/1958',
-      nomeMae: 'Ivanilde'
+      nomeMae: 'Ivanilde',
+      aposentado: true
     },
     {
       nome: 'Polyane',
-      idade: 33,
       dataNascimento: '25/04/1988',
       nomeMae: 'Sônia Maria',
-      nomePai: 'José Carlos'
+      aposentado: false
     },
     {
       nome: 'Lana',
-      idade: 1,
       dataNascimento: '18/05/2021',
       nomeMae: 'Polyane',
-    },
-    {
-      nome: 'Felipe',
-      idade: 30,
-      dataNascimento: '06/05/1990',
-      nomeMae: 'Sônia Maria',
-      nomePai: 'José Carlos'
+      aposentado: false
     }
   ]
-  
+
+  const favorito = [
+    {
+      nome: 'Felipe',
+      idade: '30 anos',
+      frutaFavorita: 'maçã',
+    },
+    {
+      nome: 'Vinicius',
+      idade: '34 anos',
+      frutaFavorita: 'abacaxi',
+    },
+    {
+      nome: 'Miguel',
+      idade: '4 anos',
+      frutaFavorita: 'abacaxi',
+    }
+  ]
+
+  const filtrandoFamilia = familia.filter(membros => {
+    return membros.aposentado === false
+  })
+
+  const filtrandoFavorito = favorito.filter(pessoas => {
+    return pessoas.frutaFavorita === 'abacaxi'
+  })
+
+
   return (
-    
-    <div className='app'>
-      {familia.map(item => {
-        return(       
-        <div className='box'>
-          <p><b>Nome:</b> {item.nome}</p>
-          <p><b>Idade:</b> {item.idade}</p>
-          <p><b>Data de Nascimento:</b> {item.dataNascimento}</p>
-          <p><b>Nome da Mãe:</b> {item.nomeMae}</p>
-          {item.nomePai && <p><b>Nome do Pai:</b> {item.nomePai}</p>}
-        </div>
-      )
-      })
-      }
-    </div>
+    <>
+      <h1>Não são Aposentados</h1>
+      <div className='app'>
+        {filtrandoFamilia.map(membros => {                          
+          return (
+            <div className='box'>
+              <p><b>Nome:</b> {membros.nome}</p>
+              <p><b>Idade:</b> {membros.dataNascimento}</p>
+              <p><b>Fruta Favorita:</b> {membros.nomeMae}</p>
+            </div>
+          )
+        })
+        }
+      </div>
+      <hr />
+      <h1>Pessoas que amam Abacaxi</h1>
+      <div className='app'>
+        {filtrandoFavorito.map(pessoas => {
+          return (
+            <div className='box'>
+              <p><b>Nome:</b> {pessoas.nome}</p>
+              <p><b>Data de Nascimento:</b> {pessoas.idade}</p>
+              <p><b>Nome da Mãe:</b> {pessoas.frutaFavorita}</p>
+            </div>
+          )
+        })
+        }
+      </div>
+    </>
   );
 }
 
