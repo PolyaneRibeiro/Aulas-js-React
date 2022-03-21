c79e7e7f2952c42be5b3898f749289c1770461e3
Polyane <poly.pequena@gmail.com>
2022-03-21 19:55:11 -0300

    presencial04

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,58 +1,68 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-
   const familia = [
     {
-      nome: 'José Carlos',
-      idade: 62,
-      dataNascimento: '12/03/1958',
-      nomeMae: 'Maria',
-      nomePai: false
+      nome: "José Carlos",
+      dataNascimento: "12/03/1958",
+      nomeMae: "Maria",
+      gostaFazer: [
+        {
+          muito: "Futebol",
+          pouco: "Nadar",
+        },
+        {
+          muito: "Basquete",
+          pouco: "Andar de Blicicleta",
+        },
+      ],
     },
     {
-      nome: 'Sônia Maria',
-      idade: 61,
-      dataNascimento: '03/09/1958',
-      nomeMae: 'Ivanilde'
+      nome: "Sônia Maria",
+      dataNascimento: "03/09/1958",
+      nomeMae: "Ivanilde",
+      gostaFazer: [
+        {
+          muito: "Futebol",
+          pouco: "Nadar",
+        },
+        {
+          muito: "Basquete",
+          pouco: "Ler",
+        },
+      ],
     },
-    {
-      nome: 'Polyane',
-      idade: 33,
-      dataNascimento: '25/04/1988',
-      nomeMae: 'Sônia Maria',
-      nomePai: 'José Carlos'
-    },
-    {
-      nome: 'Lana',
-      idade: 1,
-      dataNascimento: '18/05/2021',
-      nomeMae: 'Polyane',
-    },
-    {
-      nome: 'Felipe',
-      idade: 30,
-      dataNascimento: '06/05/1990',
-      nomeMae: 'Sônia Maria',
-      nomePai: 'José Carlos'
-    }
-  ]
-  
+  ];
+
   return (
-    
-    <div className='app'>
-      {familia.map(item => {
-        return(       
-        <div className='box'>
-          <p><b>Nome:</b> {item.nome}</p>
-          <p><b>Idade:</b> {item.idade}</p>
-          <p><b>Data de Nascimento:</b> {item.dataNascimento}</p>
-          <p><b>Nome da Mãe:</b> {item.nomeMae}</p>
-          {item.nomePai && <p><b>Nome do Pai:</b> {item.nomePai}</p>}
-        </div>
-      )
-      })
-      }
+    <div className="app">
+      {familia.map((item) => {
+        const filtrandoFamilia = item.gostaFazer.filter(membros => {
+          return membros.muito !== 'Futebol'
+        })
+          console.log(filtrandoFamilia, "filtrandoFamilia");
+        return (
+          <div className="box">
+            <p>
+              <b>Nome:</b> {item.nome}
+            </p>
+            <p>
+              <b>Data de Nascimento:</b> {item.dataNascimento}
+            </p>
+            <p>
+              <b>Nome da Mãe:</b> {item.nomeMae}
+            </p>
+            <br />
+            <p><b>Gosta de fazer</b></p>
+            {filtrandoFamilia.map(filter =>
+            <>
+            <p>{filter.muito}</p>
+            <p>{filter.pouco}</p>
+            </>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
