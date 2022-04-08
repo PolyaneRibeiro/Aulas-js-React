dae26ed4052bb49456b9f0a99e37a47c002da24e
Polyane Ribeiro <poly.pequena@gmail.com>
2022-04-08 13:39:46 -0300

    funcao

---
+++ my-app/src/utils/index.jsx
@@ -0,0 +1,7 @@
+export const MsgConsole = (valorDigitado) => {
+    console.log(`${valorDigitado} é muito louco`)
+}
+
+export const Click = (nome, cargo) => {
+    alert(`O ${nome} é o ${cargo} mais TOP`)
+}
