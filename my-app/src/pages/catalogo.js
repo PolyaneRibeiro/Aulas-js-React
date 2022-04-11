d848523b700d5c371ed0a6a97342af761de93c7f
Polyane <poly.pequena@gmail.com>
2022-04-11 20:11:16 -0300

    presencial07

--- my-app/src/pages/catalogo.js
+++ my-app/src/pages/catalogo.js
@@ -6,9 +6,7 @@ function Catalogo() {
   return (
     <>
       <h1>Catalogo</h1>
-      <div>
-        <p onClick={() => link("/moda-feminina")}>Moda Feminina</p>
-      </div>
+      <p onClick={() => link("/moda-feminina")}>Moda Feminina</p>
       <p onClick={() => link("/moda-masculina")}>Moda Maculina</p>
       <p onClick={() => link("/moda-infantil")}>Moda Infantil</p>
       <p onClick={() => link("/moda-nerd")}>Moda Nerd</p>
