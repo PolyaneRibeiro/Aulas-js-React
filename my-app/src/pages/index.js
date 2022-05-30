6934ad083ff0e07c1e57086a0dfe1dfd1f9b9c99
Douglas <douglasflorentinosantos@gmail.com>
2022-05-30 19:17:28 -0300

    presencial12

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -2,27 +2,46 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 
 function Home() {
-  const [series, setSeries] = useState()
+  const [pokemon, setPokemon] = useState()
+  const [card, setCard] = useState()
+  const [modal, setModal] = useState(false)
 
   useEffect(() => {
-    axios.get('https://streaming-d95a2-default-rtdb.firebaseio.com/series.json')
-      .then((response) => setSeries(response.data))
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=150&offset=0')
+      .then((response) => setPokemon(response.data.results))
   }, []);
 
+  const modalPokemon = (id) => {
+    setModal(true);
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => setCard(response.data));
+  };
+
+  console.log(card)
   return (
     <div className="content">
-      <h1>Aula de Get</h1>
-      <div className="app">
-        {series && Object.values(series).map(item => {
-          return (
-            <div className="box">
-              <h3>Titulo: {item.titulo}</h3>
-              <p>Gênero: {item.genero}</p>
-              <p>Temporadas: {item.temporadas}</p>
-            </div>
-          )
-        })}
-      </div>
+      <h1>Pokemon</h1>
+      {modal && (
+        <>
+          <p>{card?.name}</p>
+          <p>Altura:{card?.height}</p>
+          <p>Peso:{card?.weight}</p>
+          {card?.types.map(pokeTypes => {
+            return (
+              <p>{pokeTypes.type.name}</p>
+            )
+          }
+          )}
+        </>
+      )}
+      {pokemon?.map((item, index) => {
+        const id = index + 1
+        return (
+          <h3 onClick={() => modalPokemon(id)}>{item.name}</h3>
+        )
+      })}
+
     </div>
   );
 }
