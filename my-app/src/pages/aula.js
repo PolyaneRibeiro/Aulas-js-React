af0ebd7d0a6d4d85628927e7f46b3bdf6c2b0677
Polyane <poly.pequena@gmail.com>
2022-06-05 23:24:25 -0300

    postPatchDelete

--- my-app/src/pages/aula.js
+++ my-app/src/pages/aula.js
@@ -4,36 +4,77 @@ import axios from 'axios'
 
 function Aula() {
     const [resposta, setResposta] = useState()
-    const [respostaAxios, setRespostaAxios] = useState()
+    const [tipo, setTipo] = useState()
+    const [valor, setValor] = useState()
+    const [cor, setCor] = useState()
+    const [dadoEditado, setDadoEditado] = useState()
+    const [mapeamento, setMapeamento] = useState()
 
 
     useEffect(() => {
         fetch('https://teste-aula-metodos-3b964-default-rtdb.firebaseio.com/roupas.json')
             .then(response => response.json())
             .then(data => setResposta(data))
-    }, []);
-
-    useEffect(() => {
-        axios.get('https://teste-aula-metodos-3b964-default-rtdb.firebaseio.com/roupas.json')
-            .then((response) => setRespostaAxios(response.data))
-    }, []);
+    }, [mapeamento]);
 
 
-    console.log(resposta, 'roupas')
-    console.log(respostaAxios, 'roupas Axios')
+    const cadastro = () => {
+        axios.post('https://teste-aula-metodos-3b964-default-rtdb.firebaseio.com/roupas/femininas.json', {
+            cor: cor,
+            tipo: tipo,
+            valor: valor
+        })
+            .then(() => {
+                alert('sua roupa foi cadastrada')
+                setMapeamento(!mapeamento)
+            })
+            .catch(() => alert('não conseguiu cadastrar'))
+    }
+
+    const deletar = (id) => {
+        axios.delete(`https://teste-aula-metodos-3b964-default-rtdb.firebaseio.com/roupas/femininas/${id}.json/`, {})
+            .then(() => {
+                alert('sua roupa foi excluida')
+                setMapeamento(!mapeamento)
+            })
+            .catch(() => alert('não conseguiu excluir a roupa'))
+    }
+
+    const editar = (id) => {
+        axios.patch(`https://teste-aula-metodos-3b964-default-rtdb.firebaseio.com/roupas/femininas/${id}.json/`, {
+            tipo: dadoEditado
+        })
+            .then(() => {
+                alert('sua roupa foi editada')
+                setMapeamento(!mapeamento)
+            })
+            .catch(() => alert('não conseguiu editar a roupa'))
+    }
+
 
     return (
         <>
-
-            {resposta?.masculina.tipo}
-            {respostaAxios?.masculina.tipo}
-
-            {respostaAxios && Object.values(respostaAxios.femininas).map(roupa => {
+            <h2>Roupas Femininas</h2>
+            {resposta && Object.entries(resposta.femininas).map(roupa => {
+                console.log(roupa, 'roupa')
                 return (
-                    <p>{roupa.tipo}</p>
+                    <div className="box">
+                        {roupa[1].tipo}
+                        <input onChange={(e) => setDadoEditado(e.target.value)} />
+                        <button onClick={() => deletar(roupa[0])}>Excluir</button>
+                        <button onClick={() => editar(roupa[0])}>Editar</button>
+                        {/* <p>{roupa.valor}</p>
+                    <p>{roupa.cor}</p> */}
+                    </div>
                 )
             })}
-
+            <h2>Cadastrar nova roupa</h2>
+            <form>
+                <label>Tipo:</label><input onChange={(e) => setTipo(e.target.value)} /><br />
+                <label>Valor:</label><input onChange={(e) => setValor(e.target.value)} /><br />
+                <label>Cor:</label><input onChange={(e) => setCor(e.target.value)} /><br />
+                <button onClick={(e) => { e.preventDefault(); cadastro() }}>Cadastrar</button>
+            </form>
         </>
     );
 }
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -3,26 +3,74 @@ import axios from 'axios'
 
 function Home() {
   const [series, setSeries] = useState()
+  const [mapeamento, setMapeamento] = useState()
+  const [dadoEditado, setDadoEditado] = useState()
+  const [titulo, setTitulo] = useState()
+  const [genero, setGenero] = useState()
+  const [temporadas, setTemporadas] = useState()
 
   useEffect(() => {
     axios.get('https://streaming-d95a2-default-rtdb.firebaseio.com/series.json')
       .then((response) => setSeries(response.data))
-  }, []);
+  }, [mapeamento]);
+
+  const cadastro = () => {
+    axios.post('https://streaming-d95a2-default-rtdb.firebaseio.com/series.json', {
+      titulo: titulo,
+      genero: genero,
+      temporadas: temporadas
+    })
+      .then(() => {
+        alert('sua série foi cadastrada')
+        setMapeamento(!mapeamento)
+      })
+      .catch(() => alert('não foi possível cadastrar a série'))
+  }
+
+  const deletar = (id) => {
+    axios.delete(`https://streaming-d95a2-default-rtdb.firebaseio.com/series/${id}.json`, {})
+      .then(() => {
+        alert('sua série foi excluida')
+        setMapeamento(!mapeamento)
+      })
+      .catch(() => alert('não foi possível excluir a série'))
+  }
+
+  const editar = (id) => {
+    axios.patch(`https://streaming-d95a2-default-rtdb.firebaseio.com/series/${id}.json`, {
+      titulo: dadoEditado
+    })
+      .then(() => {
+        alert('sua série foi editada')
+        setMapeamento(!mapeamento)
+      })
+      .catch(() => alert('não foi possível editar a série'))
+  }
 
   return (
     <div className="content">
-      <h1>Aula de Get</h1>
+      <h1>Aula de Post, Patch e Delete</h1>
       <div className="app">
-        {series && Object.values(series).map(item => {
+        {series && Object.entries(series).map(item => {
           return (
             <div className="box">
-              <h3>Titulo: {item.titulo}</h3>
-              <p>Gênero: {item.genero}</p>
-              <p>Temporadas: {item.temporadas}</p>
+              <h3>Título: {item[1].titulo}</h3>
+              <p>Gênero: {item[1].genero}</p>
+              <p>Temporadas: {item[1].temporadas}</p>
+              <input onChange={(e) => setDadoEditado(e.target.value)} />
+              <button onClick={() => deletar(item[0])}>Excluir</button>
+              <button onClick={() => editar(item[0])}>Editar</button>
             </div>
           )
         })}
       </div>
+      <h2>Cadastrar nova série</h2>
+      <form>
+        <label>Título:</label><input onChange={(e) => setTitulo(e.target.value)} /><br />
+        <label>Gênero:</label><input onChange={(e) => setGenero(e.target.value)} /><br />
+        <label>Temporadas:</label><input onChange={(e) => setTemporadas(e.target.value)} /><br />
+        <button onClick={(e) => { e.preventDefault(); cadastro() }}>Cadastrar</button>
+      </form>
     </div>
   );
 }
